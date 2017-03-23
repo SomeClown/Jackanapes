7d6065d925a45f6b4fb035cd7d462c41e96b0904
SomeClown <justsomeclown@gmail.com>
2017-03-23 15:10:49 -0700

    app directory structure changes

---
+++ app/arg_proc.py
@@ -0,0 +1,261 @@
+#!/usr/local/bin/python3
+
+import curses
+import tweepy
+import globalVars
+
+__author__ = 'SomeClown'
+
+
+"""
+
+All of the logic for the program goes here. pq.py.py calls pq_args which is where we set up
+the parser. The parser then passes back to this file the options selected, where the appropriate
+stanza is executed. The execution logic takes place in Packet_Tweet, where each function is also
+wrapped with the authorization function.
+
+"""
+
+
+def arglebarg(command_args):
+    """
+
+    :type command_args: object
+    :type command_args: object
+    :return:
+    :param command_args:
+    :rtype: object
+
+    """
+
+    from Packet_Tweet import TweetArguments
+    my_tweet_args = TweetArguments()
+
+    if command_args.followers:
+        try:
+            my_tweet_args.save_followers(command_args.followers[0])
+        except SystemExit:
+            raise
+        except KeyboardInterrupt:
+            raise
+
+    if command_args.friends:
+        try:
+            my_tweet_args.save_friends(command_args.friends[0])
+        except SystemExit:
+            raise
+        except KeyboardInterrupt:
+            raise
+
+    if command_args.baddies:
+        try:
+            my_tweet_args.compare_followers()
+        except SystemExit:
+            raise
+        except KeyboardInterrupt:
+            raise
+
+    # Get time line with 'n' number of tweets
+    elif command_args.tweets_num:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            my_tweet_args.print_timeline(command_args.tweets_num[0])
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    # Get mentions with 'n' number of tweets
+    elif command_args.userMentions:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            my_tweet_args.print_mentions(command_args.userMentions[0])
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    # Start stream on <@username>
+    # If 'all' is put in place of username, stream user's home timeline
+    elif command_args.streamUserSearch:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            if 'all' in command_args.streamUserSearch:
+                my_tweet_args.get_stream()
+            else:
+                screen_name = command_args.streamUserSearch[0]
+                my_tweet_args.get_follow_stream(screen_name)
+        except tweepy.TweepError as e:
+            curses.endwin()
+            print(e.response)  # This works
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    # Start stream using <search term>
+    elif command_args.search:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            searchTerm = command_args.search
+            my_tweet_args.get_stream_search(searchTerm)
+        except tweepy.TweepError:
+            curses.endwin()
+            print(tweepy.TweepError)
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    # Print <n> number of friends to screen, where <n> is less than Titter max limit
+    elif command_args.numFriends:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            my_tweet_args.print_friends(command_args.numFriends[0])
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    # send a direct message
+    elif command_args.directMessage:
+        userDirect = command_args.directMessage[0]
+        msgDirect = command_args.directMessage[1]
+        my_tweet_args.direct_send(userDirect, msgDirect)
+
+    # update status
+    elif command_args.status_update:
+        msg_status_update = command_args.status_update[0]
+        my_tweet_args.status_update(msg_status_update)
+
+    elif command_args.myInfo:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()  # Keeps key presses from echoing to screen
+            curses.cbreak()  # Takes input away
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)  # Foreground Red/background transparent
+            curses.init_pair(2, curses.COLOR_YELLOW, -1)
+            curses.init_pair(3, curses.COLOR_BLUE, -1)
+            curses.init_pair(5, curses.COLOR_MAGENTA, -1)
+            my_tweet_args.print_my_info()
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    elif command_args.notMe:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()
+            curses.cbreak()
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)
+            my_tweet_args.print_not_me(command_args.notMe[0])
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    elif command_args.retweets:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()
+            curses.cbreak()
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)
+            my_tweet_args.print_retweets(command_args.retweets[0])
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+    elif command_args.term:
+        globalVars.screen = curses.initscr()
+        try:
+            globalVars.screen.scrollok(True)
+            curses.noecho()
+            curses.cbreak()
+            globalVars.screen.keypad(1)
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_RED, -1)
+            my_tweet_args.term_search(command_args.term)
+        except SystemExit:
+            curses.endwin()
+            raise
+        except KeyboardInterrupt:
+            curses.endwin()
+            raise
+
+
+    else:
+        return
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
+else:
+    pass
---
+++ app/authorization.py
@@ -0,0 +1,106 @@
+#!/usr/local/bin/python3
+
+import os
+import globalVars
+import tweepy
+from functools import wraps
+from utilities import logging_wrapper
+import yaml
+
+__author__ = 'SomeClown'
+
+
+def set_config():
+
+    # Load and assign key variables from yaml configuration file
+    my_config_file = open('config.yml')
+    settings = yaml.load(my_config_file)
+    globalVars.access_token = settings['access_token']
+    globalVars.access_token_secret = settings['access_token_secret']
+    globalVars.consumer_token = settings['consumer_token']
+    globalVars.consumer_token_secret = settings['consumer_token_secret']
+    globalVars.user = settings['user']
+    globalVars.home = settings['home']
+    globalVars.followers = settings['followers']
+    globalVars.friend_file = settings['friend_file']
+    globalVars.no_follow = settings['no_follow']
+
+
+@logging_wrapper
+def initial_auth(original: object) -> object:
+    """
+    :type original: object
+    :rtype: object
+    """
+    @wraps(original)
+    def wrapper(*args, **kwargs):
+        # TODO: Write test method to test for auth without constantly re-authing with twitter
+        set_config()
+        globalVars.auth = tweepy.OAuthHandler(globalVars.consumer_token, globalVars.consumer_token_secret)
+        globalVars.api = tweepy.API(globalVars.auth)
+        globalVars.user_id = globalVars.api.get_user(globalVars.user)
+        home = os.path.expanduser("~")
+        config_file = (home + '/.packetqueue/' + str(globalVars.user_id.screen_name) + '/.packetqueue')
+
+        # Try to load with access token, if that doesn't work we must not have valid credentials
+        # and we begin the process to call out to Twitter and get new credentials assigned
+        try:
+            globalVars.auth.set_access_token(globalVars.access_token, globalVars.access_token_secret)
+
+        # Move through granting access token process
+        except IOError:
+            print('Invalid or missing credentials... \n')
+
+            try:
+                redirect_url = globalVars.auth.get_authorization_url()
+                print('If a new browser window doesn\'t open, go to this URL: ')
+                print(redirect_url)
+                print('and authorize this app. Return here with the pin code you receive in order to finish')
+                print('authorizing this app to access your account as specified.')
+                import webbrowser
+                assert isinstance(redirect_url, object)
+                webbrowser.open_new_tab(redirect_url)
+                verify_pin = input('Pin Code: ')
+                assert isinstance(verify_pin, object)
+                print(verify_pin)
+                try:
+                    globalVars.auth.get_access_token(verify_pin)
+                except tweepy.TweepError:
+                    print('Error! Failed to get access token, or incorrect token was entered.')
+                    return 1
+
+                access_token = globalVars.auth.access_token
+                access_token_secret = globalVars.auth.access_token_secret
+
+                # Write all of this good authentication stuff to a file
+                # so we don't have to do it everytime we run the program
+                # TODO: Change this to use os.mkdirs() so this is more concise
+                home = ''
+                ifconfig_path = os.path.join(home, '/.packetqueue/', str(globalVars.user.screen_name))
+                if not os.path.exists(home + '/.packetqueue/'):
+                    os.mkdir(home + '/.packetqueue/')
+                    if not os.path.exists(home + '/.packetqueue/' + str(globalVars.user.screen_name)):
+                        os.mkdir(home + '/.packetqueue/' + str(globalVars.user.screen_name))
+
+                if_config_file = (home + '/.packetqueue/' + str(globalVars.user.screen_name) + '/.packetqueue')
+                print(if_config_file)
+                with open(if_config_file, 'w+') as outFile:
+                    outFile.write(access_token + '\n')  # function as a better way to store
+                    outFile.write(access_token_secret + '\n')  # the data
+
+            # Something is so horribly borked we're just going to say fuck it
+            except tweepy.TweepError:
+                print('Error! Failed to get request token.')
+                return (1)
+        return original(*args, **kwargs)
+
+    assert isinstance(wrapper, object)
+    return wrapper
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
+else:
+    pass
